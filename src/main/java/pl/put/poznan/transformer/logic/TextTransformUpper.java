1eb64df99aa9079c3336aa417a25ead70db5f317
kowalewskiadam <adam.kowalewski@student.put.poznan.pl>
2021-12-18 11:50:03 +0100

    Pierwszy opis klasy w dokumentacji

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformUpper.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformUpper.java
@@ -1,5 +1,13 @@
 package pl.put.poznan.transformer.logic;
-
+/**
+ * Klasa TextTransformUpper służy do zamiany wszystkich liter z małych na duże.
+ * TextTransformUpper implementuje interfejs TextTransform.
+ * Na przykład gdy na wejściu pojawi się tekst "inzynieria oprogramowania" na
+ * wyjściu otrzymamy tekst "INZYNIERIA OPROGRAMOWANIA".
+ *
+ * @author Me
+ * @version 1.0
+ */
 public class TextTransformUpper implements TextTransform {
 
     private final String transforms;
